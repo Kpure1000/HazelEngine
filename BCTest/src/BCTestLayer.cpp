df083c7f08e92b46018fa8069eaa39788a61da5d
Kpure1000 <ktsq2000@126.com>
2021-04-21 02:01:30 +0800

    update: complete FAKE test

--- BCTest/src/BCTestLayer.cpp
+++ BCTest/src/BCTestLayer.cpp
@@ -19,27 +19,110 @@ void BCTestLayer::OnDetch()
 
 void BCTestLayer::OnUpdate()
 {
-	static int count = 0;
-	Log::Info("BCTest: {0}.", count++);
+	RenderCommand::Clear();
+	// MultiThread Test
+	{
+		HZ_PROFILE_SCOPE("Efective Tester");
+		static int queryCount = 0;
+		static int checkCount = 0;
+		static int loadUpCount = 0;
+		static float queryTime = 0.0f;
+		static float checkTime = 0.0f;
+		static float loadUpTime = 0.0f;
+		if (queryTime > 4.3f)
+		{
+			Log::Debug("BCTest>> Query Times: {0}.", queryCount++);
+			queryTime = 0.0f;
+		}
+		queryTime += Time::deltaTime();
+		if (checkTime > 13.3f)
+		{
+			Log::Trace("BCTest>> Check Times: {0}.", checkCount++);
+			checkTime = 0.0f;
+		}
+		checkTime += Time::deltaTime();
+		if (loadUpTime > 19.7f)
+		{
+			Log::Warn("BCTest>> Load Up Times: {0}.", loadUpCount++);
+			loadUpTime = 0.0f;
+		}
+		loadUpTime += Time::deltaTime();
+		std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 100));
+	}
+
+	{
+		HZ_PROFILE_SCOPE("Consensus Tester");
+	}
+
 }
 
 void BCTestLayer::OnImGuiRender()
 {
 	HZ_PROFILE_FUNCTION();
-	ImGui::Begin("BCTest Properties");
-	//  TODO
-	ImGui::Text("%lld DrawCalls", SpriteRenderer::GetState().DrawCalls);
-	static float ddt = 400.0f;
-	static float framePS = 0.0f;
-	ddt += Time::deltaTime();
-	if (ddt > 1.0f) {
-		framePS = 1.0f / Time::deltaTime();
-		ddt = 0.0f;
+	{
+		ImGui::Begin("BCTest Properties");
+		{
+			ImGui::Text("Test Option:");
+			static int curItem = 0;
+			ImGui::Combo("Test Mode Select", &curItem, m_ItemName, 4);
+
+			static bool isStateChannel;
+			ImGui::Checkbox("Open State Channel", &isStateChannel);
+		}
+		{
+			ImGui::Text("\nBlock Efective Test:");
+			static int ths = 0;
+			ImGui::SliderInt("Test Thread Numbers", &ths, 1, 24);
+		}
+		{
+			ImGui::Text("\nTrade:");
+			static int fake = 0;
+			ImGui::SliderInt("Random Fake Block", &fake, 0, 30);
+		}
+		ImGui::End();
 	}
-	ImGui::Text("%f FPS", framePS);
-	ImGui::End();
 }
 
 void BCTestLayer::OnEvent(Event& e)
 {
+	hazel::EventDispatcher dispatcher(e);
+	dispatcher.Dispatch<KeyPressedEvent>([this](KeyPressedEvent& ev)->bool
+		{
+			HZ_PROFILE_SCOPE("Deal Suspected Block");
+			if (ev.GetKeyCode() == Key::Enter)
+			{
+				// Trade Test
+				{
+					switch (rand() % 5)
+					{
+					case 0: Log::Warn("Hash Reply From {0}: Bytes={1} Internal={2}ms Wrong={3}",
+						addA, 256, rand() % 9000 + 4000, "False"); break;
+					case 1: Log::Warn("Hash Reply From {0}: Bytes={1} Internal={2}ms Wrong={3}",
+						addB, 256, rand() % 9000 + 4000, "False"); break;
+					case 2: Log::Warn("Hash Reply From {0}: Bytes={1} Internal={2}ms Wrong={3}",
+						addC, 256, rand() % 9000 + 4000, "False"); break;
+					case 3: Log::Warn("Hash Reply From {0}: Bytes={1} Internal={2}ms Wrong={3}",
+						addD, 256, rand() % 9000 + 4000, "False"); break;
+					case 4: Log::Warn("Hash Reply From {0}: Bytes={1} Internal={2}ms Wrong={3}",
+						addE, 256, rand() % 9000 + 4000, "False"); break;
+					default:
+						break;
+					}
+				}
+			}
+			if (ev.GetKeyCode() == Key::Backspace)
+			{
+				switch (rand() % 5)
+				{
+				case 0: Log::Warn("Find a Block {0} seems like TROJAN", addA); break;
+				case 1: Log::Warn("Find a Block {0} seems like TROJAN", addB); break;
+				case 2: Log::Warn("Find a Block {0} seems like TROJAN", addC); break;
+				case 3: Log::Warn("Find a Block {0} seems like TROJAN", addD); break;
+				case 4: Log::Warn("Find a Block {0} seems like TROJAN", addE); break;
+				default:
+					break;
+				}
+			}
+			return false;
+		});
 }
--- BCTest/src/BCTestLayer.h
+++ BCTest/src/BCTestLayer.h
@@ -16,6 +16,11 @@ public:
 	virtual void OnEvent(Event& e) override;
 	
 private:
-	// TODO: 
+	const char* m_ItemName = "Efective Test\0Trade Success Rate\0Consensus Rate\0Hack Test";
+	const string addA = "d3c1ef4b4a6f6fe6466fe6c322daf4ce";
+	const string addB = "3b4eacb34c4ba5f6fe64b4a69c3ff234";
+	const string addC = "46b4b2daacba5fb34a91652ef032d3cc";
+	const string addD = "2fc4e094b06fe692da3c34178d3b34ba";
+	const string addE = "a34dac3aba5fbacba50cdf3da3f4b906";
 };
 
